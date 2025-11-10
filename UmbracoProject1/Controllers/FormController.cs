408d7efffeba0765eb46884fe44ff2dccd8f47d4
abdifatah7 <abdifatahmahdi26@gmail.com>
2025-11-10 15:20:51 +0100

    Created working forms on the homepage, as well as adjusted CSS according to design.

---
+++ UmbracoProject1/Controllers/FormController.cs
@@ -0,0 +1,42 @@
+﻿using UmbracoProject1.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Org.BouncyCastle.Security;
+using UmbracoProject1.Services;
+
+namespace UmbracoProject1.Controllers
+{
+    public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissionService formSubmissions) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+    {
+        private readonly FormSubmissionService _formSubmissions = formSubmissions;
+
+        public IActionResult HandleCallbackForm(CallbackFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            var result = _formSubmissions.SaveCallbackRequest(model);
+
+            if (!result)
+            {
+                TempData["FormError"] = "Something went wrong while submitting your request. Please try again later.";
+                return RedirectToCurrentUmbracoPage();
+            }
+
+            TempData["FormSuccess"] = "Thank you, your request has been recived and we will get back to you soon.";
+            return RedirectToCurrentUmbracoPage();
+        }
+ 
+    }
+
+
+}
+
